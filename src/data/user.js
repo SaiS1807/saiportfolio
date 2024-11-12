cde2c3e9a7cf7e840f7b8c5ada29609d7a39e896
unknown <sai.surya1807@gmail.com>
2024-11-12 13:36:36 +0530

    Initial commit

---
+++ src/data/user.js
@@ -0,0 +1,68 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Sai",
+		name: "Sai Suryavanshi",
+		email: "sai.surya1807@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		//twitter: "https://twitter.com/",
+		//github: "https://github.com/",
+		linkedin: "https://www.linkedin.com/in/sai-suryavanshi-38509b291?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app ",
+		instagram: "https://www.instagram.com/saisuryavanshi_1807?igsh=c21iNjJkeDVqNzQ2",
+		//stackoverflow: "https://stackoverflow.com/",
+		//facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web",
+		description:
+			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Sai Sampat Suryavanshi and I'm Frontend developer.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Direct messaging app",
+			description:
+				" Developed a direct messaging application using Java, enabling realtime communication ",
+			//logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			//linkText: "View Project",
+			//link: "https://github.com",
+		},
+
+		{
+			title: "Age Detection",
+			description:
+				"Developed a real-time age detection system using Python ",
+			//logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			//linkText: "View Project",
+			//link: "https://github.com",
+		},
+
+		{
+			title: "Prediction of Cardiovascular diseases",
+			description:
+				" Developed a predictive model to identify the risk of cardiovascular disease",
+			//logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			//linkText: "View Project",
+			//link: "https://github.com",
+		},
+
+		
+	],
+};
+
+export default INFO;
